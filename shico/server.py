5d3f202d1de6209a88d302025c86d90bf3bc1a8b
Carlos Martinez <c.martinez@esciencecenter.nl>
2015-12-09 17:16:09 +0100

    Test, bugs and docs

--- shico/server.py
+++ shico/server.py
@@ -28,17 +28,18 @@ trackParser.add_argument('minDist', type=float, default=0.0)
 trackParser.add_argument('wordBoost', type=float, default=1.0)
 trackParser.add_argument('forwards', type=bool, default=True)
 trackParser.add_argument('sumDistances', type=bool, default=False)
-trackParser.add_argument('outlinks', type=bool, default=False)
+trackParser.add_argument('algorithm', type=str, default='inlinks')
 
 if __name__ == '__main__':
     app = Flask(__name__)
-    # app.debug = True
+    app.debug = True
 
-    @app.route('/track/<word>')
-    def trackWord(word):
+    @app.route('/track/<terms>')
+    def trackWord(terms):
         defaults = trackParser.parse_args()
+        termList = terms.split(',')
         results = _vm. \
-            trackClouds(word, maxTerms=defaults['maxTerms'],
+            trackClouds(termList, maxTerms=defaults['maxTerms'],
                         maxRelatedTerms=defaults['maxRelatedTerms'],
                         startKey=defaults['startKey'],
                         endKey=defaults['endKey'],
@@ -46,7 +47,7 @@ if __name__ == '__main__':
                         wordBoost=defaults['wordBoost'],
                         forwards=defaults['forwards'],
                         sumDistances=defaults['sumDistances'],
-                        outlinks=defaults['outlinks'],
+                        algorithm=defaults['algorithm'],
                         )
         return jsonify(results)
 
--- tests/testUtils.py
+++ tests/testUtils.py
@@ -4,6 +4,7 @@ import numpy as np
 
 
 class TestUtils(unittest.TestCase):
+    '''Tests for utils'''
     @classmethod
     def setUpClass(self):
         windowSize = 15
@@ -14,27 +15,33 @@ class TestUtils(unittest.TestCase):
         self.yRange = np.linspace(self.y0, self.yN)
 
     def testJSD(self):
-        self._doTests(shU.weightJSD)
+        '''Test JSD weighting function'''
+        self._doTests(shU.weightJSD, 'JSD')
 
     def testGaussian(self):
-        self._doTests(shU.weightGauss)
-        self._doTests(lambda y1, y2: shU.weightGauss(y1, y2, c=5))
+        '''Test Gaussian weighting function'''
+        self._doTests(shU.weightGauss, 'Gaussian')
+        # Test with non-default C
+        self._doTests(lambda y1, y2: shU.weightGauss(y1, y2, c=5), 'Gaussian')
 
     def testLinear(self):
-        self._doTests(shU.weightLinear)
-        self._doTests(lambda y1, y2: shU.weightLinear(y1, y2, a=5))
+        '''Test linear weighting function'''
+        self._doTests(shU.weightLinear, 'Linear')
+        # Test with non-default A
+        self._doTests(lambda y1, y2: shU.weightLinear(y1, y2, a=5), 'Linear')
 
-    def _doTests(self, f):
+    def _doTests(self, f, name):
+        ''' Apply sanity checks to the given weighting function. '''
         self.assertEqual(f(self.y1, self.y1), 1,
-                         'Should be 1 for the same number')
+                         name + ' should be 1 for the same number')
         self.assertEqual(f(self.y1, self.y1), 1,
-                         'Should be symmetric')
+                         name + ' should be symmetric')
         self.assertGreater(f(self.y1, self.y1), 0,
-                           'Should be positive')
+                           name + ' should be positive')
 
         # Test function in a range
         wRange = np.array([f(self.y1, yi) for yi in self.yRange])
         self.assertLessEqual(wRange.max(), 1,
-                             'Should have upper bound 1')
+                             name + ' should have upper bound 1')
         self.assertGreaterEqual(wRange.min(), 0,
-                                'Should have lower bound 0')
+                                name + ' should have lower bound 0')
