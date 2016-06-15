01a8c1079b377d49e79961f7779e44f781f87a02
Carlos Martinez <c.martinez@esciencecenter.nl>
2016-06-15 14:23:00 +0200

    Times script

---
+++ shico/scripts/times_parsing.py
@@ -0,0 +1,74 @@
+
+from BeautifulSoup import BeautifulSoup
+import re
+import nltk
+import string
+from HTMLParser import HTMLParser
+
+# Helper functions
+
+def getText(textElement):
+    regex = re.compile('\ +')
+    html = HTMLParser()
+
+    text = regex.sub(' ', textElement)
+    text = text.strip()
+    text = html.unescape(text)
+
+    return text
+
+def getTitle(article):
+    title = article.find('text.title')
+    if title is None:
+        return ''
+    titleText = title.getText(' ')
+    return getText(titleText)
+
+def getBody(article):
+    crs = article.findAll('text.cr')
+    crTexts = [ getText(cr.getText(' ')) for cr in crs ]
+    return ' '.join(crTexts)
+
+def getSoupFromXML(xmlfile):
+    with open(datafile,'r') as fin:
+        data = fin.read()
+    soup = BeautifulSoup(data)
+    return soup
+
+def getArticlesFromSoup(soup):
+    articles = []
+    for article in soup.findAll('article'):
+        title = getTitle(article)
+        body = getBody(article)
+        articles.append((title, body))
+    return articles
+
+# Example file load
+# datafile = 'data/times-20101217/0FFO-2010-1217.xml'
+# soup = getSoupFromXML(datafile)
+# articles = getArticlesFromSoup(soup)
+# print len(articles)
+# title, body = articles[100]
+# print 'Title  :',title
+# print 'Content:\n', body
+
+
+# ## Load XML and save as CSV
+from glob2 import glob
+import pandas as pd
+
+origPath = '../data/times-20101217/'
+savePath = './'
+
+# Save / load as CSV
+for datafile in glob(origPath + '0FFO*.xml'):
+    print 'Loading   : ',datafile
+    saveFile = datafile.replace(origPath, savePath)
+    saveFile = saveFile.replace('.xml', '.csv')
+    print '...save as: ',saveFile
+
+    soup = getSoupFromXML(datafile)
+    articles = getArticlesFromSoup(soup)
+
+    df = pd.DataFrame(articles, columns=['Title', 'Content'])
+    df.to_csv(saveFile, encoding='UTF8')
