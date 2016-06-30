6c0ed499e586ad2407383eeb1eeb82d9f9cbca06
Carlos Martinez <c.martinez@esciencecenter.nl>
2016-06-30 11:32:48 +0200

    Update slider when expanded - fixes #26

--- webapp/src/app/components/shico/trackerParameters.controller.js
+++ webapp/src/app/components/shico/trackerParameters.controller.js
@@ -5,22 +5,34 @@
     .module('shico')
     .controller('TrackerParametersController', TrackerParametersController);
 
-  function TrackerParametersController(TrackerParametersService) {
+  function TrackerParametersController($scope, $timeout, TrackerParametersService) {
     var vm = this;
+
+    // Values for dropdown items
     vm.algorithms = ['Adaptive', 'Non-adaptive'];
     vm.weighFuncs = ['Gaussian', 'Linear', 'JSD'];
     vm.directions = ['Forward', 'Backward'];
     vm.boostMethods = ['Sum similarity', 'Counts'];
     vm.doCleaning = [ 'Yes', 'No' ];
 
-    // Years and features gets loaded by SettingsService
+    // Open/close advanced parameters section
+    vm.isOpen = false;
+
+    // We use variables from TrackerParametersService directly.
     vm.years = TrackerParametersService.availableYears;
     vm.features = TrackerParametersService.features;
-
-    // We use the parameters variable from ParameterService directly.
     vm.parameters = TrackerParametersService.getParameters();
-
-    //
     vm.tooltips = TrackerParametersService.tooltips;
+
+    // Expose functions
+    vm.toggleOpen = toggleOpen;
+
+    function toggleOpen() {
+      vm.isOpen = !vm.isOpen;
+      $timeout(function () {
+        // Force redraw of slider
+        $scope.$broadcast('rzSliderForceRender');
+      }, 500); // Wait 500 ms before triggering redraw
+    }
   }
 })();
